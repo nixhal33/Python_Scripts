d901d2f1cdc3a745931349756703c40db5435e66
nixhal33 <nischal.koirala33@gmail.com>
2025-03-18 16:55:44 +0545

    optimized

--- web scraping/pag_2.py
+++ web scraping/pag_2.py
@@ -3,7 +3,17 @@ from bs4 import BeautifulSoup
 import os
 
 root_website = 'https://subslikescript.com'
-secondary_website = f'{root_website}/movies'
+secondary_website = f'{root_website}/movies_letter-S'
 result = requests.get(secondary_website)
 content = result.text
 soup = BeautifulSoup(content,'lxml')
+# print(soup.prettify())
+
+# for pagination
+pagination = soup.find('ul',class_='pagination')
+pages = pagination.find_all('li', class_='page-item')
+
+# simple entire div class for the movies title and their link div
+main_box = soup.find('article', class_='main-article')
+
+
